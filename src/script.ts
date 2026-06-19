21667815f291554d7742a0826254563cc26ff156
codeNow26 <code.now26@gmail.com>
2026-06-19 10:05:01 +0200

    settings-site dynamic theme hovers, select preview and start-btn

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,14 @@
+document.getElementById("start-btn")?.addEventListener("click", () => {
+    document.getElementById("start-screen")?.classList.add("d-none");
+    document.getElementById("settings-screen")?.classList.remove("d-none");
+});
+
+document.getElementById("start-game-btn")?.addEventListener("click", () => {
+    document.getElementById("settings-screen")?.classList.add("d-none");
+    document.getElementById("game-screen")?.classList.remove("d-none");
+    console.log("geklickt")
+});
+
 export function initGame() {
     renderCards()
 }
@@ -18,3 +29,59 @@ function renderCards() {
         }
     }
 }
+
+function changeTheme() {
+    const previews = document.querySelectorAll(".theme-preview img");
+    const radioInputs = document.querySelectorAll(".radio-option");
+    let selectedImage = 0;
+
+    radioInputs.forEach((radio, i) => {
+        radio.addEventListener("mouseenter", () => {
+            previews.forEach((preview) => {
+                preview.classList.remove("active");
+            });
+            previews[i].classList.add("active");
+        });
+
+        radio.addEventListener("mouseleave", () => {
+            previews[selectedImage].classList.add("active");
+        });
+
+        radio.addEventListener("change", () => {
+            selectedImage = i;
+        });
+
+    });
+};
+
+changeTheme();
+
+
+const themes = ["Code Theme", "Gaming Theme", "DA Theme", "Food Theme"];
+const players = ["Blue", "Orange"];
+const boards = ["16 cards", "24 cards", "36 cards"];
+
+function updateSelection(
+    inputName: string,
+    outputId: string,
+    values: string[]
+) {
+    const inputs = document.querySelectorAll(`input[name="${inputName}"]`);
+    const output = document.getElementById(outputId)!;
+
+    inputs.forEach((input, i) => {
+        input.addEventListener("change", () => {
+            output.textContent = values[i];
+        });
+    });
+}
+
+updateSelection("theme", "theme", themes);
+updateSelection("player", "player", players);
+updateSelection("board", "board", boards);
+
+
+
+const cards = [
+    ""
+]
